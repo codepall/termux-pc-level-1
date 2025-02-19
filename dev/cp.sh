518cbe3f51b511dd6bec2cdec9af2e506647d7ca
codepall <minecraftnfindo@gmail.com>
2025-02-19 14:57:42 +0700

    Update and rename mv.sh to cp.sh

---
+++ dev/cp.sh
@@ -0,0 +1,11 @@
+mkdir .shortcuts
+cp termux-pc-level-1/dev/start-lxde.sh .shortcuts
+cp termux-pc-level-1/dev/start-lxqt.sh .shortcuts
+cp termux-pc-level-1/dev/start-xfce.sh .shortcuts
+cp termux-pc-level-1/dev/audio-tcp.sh .
+cp termux-pc-level-1/dev/dev.sh .
+cp termux-pc-level-1/dev/admin-lxde.sh .shortcuts
+cp termux-pc-level-1/dev/admin-lxqt.sh .shortcuts
+cp termux-pc-level-1/dev/admin-xfce.sh .shortcuts
+cp termux-pc-level-1/dev/end.sh .
+cp termux-pc-level-1/dev/end.sh .shortcuts

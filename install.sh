8c6e36a8edc3a9445ce766b20262f1e9ad484e9c
codepall <minecraftnfindo@gmail.com>
2025-02-26 14:39:55 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -24,7 +24,7 @@ proot-distro login debian -- apt install sudo -y
 proot-distro login debian -- apt install vlc -y
 proot-distro login debian -- dpkg-reconfigure tzdata
 proot-distro login debian -- adduser admin
-proot-distro login debian -- cp termux-pc-level-1/sudoers /etc
+proot-distro login debian --termux-home --shared-tmp -- cp termux-pc-level-1/sudoers /etc
 pulseaudio --start --exit-idle-time=-1 --log-target=syslog --load="module-native-protocol-tcp auth-ip-acl=127.0.0.1"
 proot-distro login debian -- export PULSE_SERVER=tcp:127.0.0.1
 bash start.sh

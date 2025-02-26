19a03f56d193e8d872d61fca1e5f8f8345028041
codepall <minecraftnfindo@gmail.com>
2025-02-26 19:09:18 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -21,10 +21,12 @@ proot-distro login debian -- apt install inkscape -y
 proot-distro login debian -- apt install cron -y
 proot-distro login debian -- apt install gimp -y
 proot-distro login debian -- apt install sudo -y
+proot-distro login debian -- apt install wget -y
 proot-distro login debian -- apt install vlc -y
 proot-distro login debian -- dpkg-reconfigure tzdata
 proot-distro login debian -- adduser admin
 proot-distro login debian --termux-home --shared-tmp -- cp termux-pc-level-1/sudoers /etc
+proot-distro login debian --user admin --termux-home --shared-tmp -- wget -qO- https://raw.githubusercontent.com/Botspot/pi-apps/master/install | bash
 pulseaudio --start --exit-idle-time=-1 --log-target=syslog --load="module-native-protocol-tcp auth-ip-acl=127.0.0.1"
 proot-distro login debian -- export PULSE_SERVER=tcp:127.0.0.1
 bash start.sh
